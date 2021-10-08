19b972079cad6defd28c5e6a4d31e1af1c85b2e6
Carey <cuijingmao12@163.com>
2021-10-09 03:11:08 +0800

    List类 ADT接口定义

--- Chapter3/List/src/List.cpp
+++ Chapter3/List/src/List.cpp
@@ -1,11 +1,11 @@
 #include "List.h"
 
-List::List()
-{
-    //ctor
-}
+//列表初始化
+template <template T> void List<T>::init() {
+    header = new ListNode<T>; //创建头哨兵节点
+    trailer = new ListNode<T>;  //创建尾哨兵节点
+    header->succ= trailer; header->pred= NULL;
+    trailer->pred = header; trailer->succ = NULL;
 
-List::~List()
-{
-    //dtor
+    _size = 0; //记录规模
 }
