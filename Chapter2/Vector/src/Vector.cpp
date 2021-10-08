129b0fddb9991daa20ea23675aea6c2d6e8b0c9f
Carey <cuijingmao12@163.com>
2021-10-09 00:33:21 +0800

    Vector类 二分查找法版本B: 两分支，不能提前退出

--- Chapter2/Vector/src/Vector.cpp
+++ Chapter2/Vector/src/Vector.cpp
@@ -191,9 +191,15 @@ template <typename T> static Rank binSearchB(T* A, T const & e, Rank lo, Rank hi
 }//有多个命中元素时，不能保证返回最秩最大者，失败时，简单返回-1，不能指示失败的位置
 
 
-
-
-
+// 二分查找算法（版本C,推荐版）
+template <typename T> static Rank binSearchC(T* A, T const& e, Rank lo, Rank hi) {
+    while( lo < hi ){ //每步迭代仅需做一次比较判断，有两个分支
+            Rank mi = ( lo + hi) >>1; //以中点为轴点
+            (e < A[mi]) ? hi = mi: lo= mi+1;  //经比较后确定深入[lo,mi) 或 （mi,hi)
+    }// 查找成功后不能提前终止
+    return --lo; //循环结束时，lo为大于e的元素的最小秩，故 lo-1即不大于e的元素的最大秩
+}//有多个命中元素时，总能保证返回秩最大者； 查找失败时，能够返回失败的位置
+// 不变性： A[0,lo)中的元素皆不大于e;A[hi,n)中的元素皆大于e
 
 
 
