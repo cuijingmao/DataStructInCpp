f3080d2fc35061e843c1c17243706d6cc32d0351
Carey <cuijingmao12@163.com>
2021-10-09 03:46:42 +0800

    List类 插入方法的实现

--- Chapter3/List/src/List.cpp
+++ Chapter3/List/src/List.cpp
@@ -29,3 +29,56 @@ ListNodePosi(T) List<T>::find(T const& e, int n, ListNodePosi(T) p) const {
     }
     return NULL:  //p越出左边界意味着区间内不含e，查找失败
 }// 失败时，返回NULL O(n)
+
+
+//插入
+
+template <typename T> ListNodePosi(T) List<T>::insertAsFirst(T const& e)
+{
+    _size++;  return header->insertAsSucc(e); //e当首节点插入
+}
+
+template <typename T> ListNodePosi(T) Lsit<T>::insertAsLast(T const& e)
+{
+    _size++; return trailer->insertAsPred(e);  //e当作尾节点插入
+}
+template <typename T> ListNodePosi(T) List<T>::insertA(ListNodePosi(T) p, T const&e)
+{
+    _size++; return p->insertAsSucc(e);  //e当作p的后继插入
+}
+template <typename T> ListNodePosi(T) List<T>::insertB(ListNodePosi(T)p, T const& e)
+{
+    _size++; return p->insertAsPred(e); //e当作p的前驱插入
+}
+
+//前插入
+// 将e紧靠当前节点之前 插入于当前节点所属列表（设有哨兵头节点header）
+template <typename T>
+ListPosiNode(T) ListNode<T>::insertAsPred(T const& e)
+{
+    LsitNodePosi(T) x = new ListNode(e,pred, this); //创建新节点
+    pred->succ=x; pred =x; //设置正向连接
+    return x; // 返回新节点的位置
+}
+
+//后插入
+// 将e紧随当前节点之后插入当前节点所属列表（设有哨兵尾节点trailer)
+template <typename T>
+ListNodePosi(T) ListNode<T>::insertAsSucc(T const& e)
+{
+    ListNodePosi(T) x = new ListNode(e,this,succ); //创建新节点
+    succ->pred =x; succ =x; //设置逆向链接
+    return x; //返回新节点的位置
+}
+
+
+
+
+
+
+
+
+
+
+
+
