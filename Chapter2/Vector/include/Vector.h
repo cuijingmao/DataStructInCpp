3b6b5942618bb73e4635dde47c48cc66faeac953
Carey <cuijingmao12@163.com>
2021-10-08 21:01:15 +0800

    Vector模板类接口定义

---
+++ Chapter2/Vector/include/Vector.h
@@ -0,0 +1,20 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+typedef int Rank; //秩
+
+#define DEFAULT_CAPACITY 20 //默认的初始容量
+
+
+
+class Vector
+{
+    public:
+        Vector();
+        virtual ~Vector();
+
+    protected:
+
+    private:
+};
+
+#endif // VECTOR_H
